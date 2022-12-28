7302a0d96a4e468c1ce524407613891ad88810f0
silasviniciuss <silasvinicius.dev@gmail.com>
2022-12-28 01:31:59 -0300

    Criando condição para dizer o estado do paciente

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,26 @@
 function calcular(){
 
+    //Criando variaveis para receber os valores do ID
     var peso = document.getElementById("peso").value;
     var altura = document.getElementById("altura").value;
 
+    //Calculo IMC
     var resultado = (peso / (altura * altura));
+
+    //Variavel para receber apenas 2 casas depois da virgula
     var resultado_final = resultado.toFixed('2')
 
-    document.getElementById("resultado").innerHTML = resultado_final;
+    //Condição para definir o estado do paciente
+    if (resultado_final < 18.50){
+        document.getElementById("resultado").innerHTML = "MAGREZA " + resultado_final;
+    } else if (resultado_final >= 18.50 && resultado_final <= 24.90) {
+        document.getElementById("resultado").innerHTML = "NORMAL " + resultado_final;
+    } else if (resultado_final > 25.00 && resultado_final <= 29.90) {
+        document.getElementById("resultado").innerHTML = "SOBREPESO " + resultado_final;
+    } else if (resultado_final >= 30.00 && resultado_final <= 39.90) {
+        document.getElementById("resultado").innerHTML = "OBESIDADE " + resultado_final;
+    } else{
+        document.getElementById("resultado").innerHTML = "OBESIDADE GRAVE " + resultado_final;
+    } 
 
 }
